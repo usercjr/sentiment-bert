e4058edf15db757ace7be801633a59876c7f6326
usercjr <92197541+usercjr@users.noreply.github.com>
2025-12-08 23:14:12 +1100

    Update inference.py

--- app/inference.py
+++ app/inference.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+app = FastAPI(title="IMDb Sentiment API", version="1.0.0")
+
+model_path = "./model"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+class InputText(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {"message": "Sentiment API is running"}
+
+@app.post("/predict")
+def predict(data: InputText):
+    inputs = tokenizer(data.text, return_tensors="pt", truncation=True, padding=True)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=1).item()
+    label = "positive" if predicted_class == 1 else "negative"
+    return {"label": label}
 
